3002c64f78cd3fb96839dbcb4ca941ed3c30678a
Alpanarani Behura <103036591+AlpanaraniBehura@users.noreply.github.com>
2025-08-04 21:37:12 +0530

    17536

--- products/views.py
+++ products/views.py
@@ -24,3 +24,43 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# Static hardcoded menu
+class MenuAPIView(APIView):
+    def get(self, request):
+        menu = [
+            {
+                "name": "Veggie Burger",
+                "description": "Grilled plant based patty with lettue,tomato and aioil",
+                "price":7.49
+
+            },
+            {
+               "name":"Caesar Salad",
+               "description":"Romaine lettuce,parmesan cheese,croutons and Caesar dressing",
+               "price": 5.99
+            },
+            {
+                "name":"Margherita Pizza",
+                "description":"Classic cheese and tomato pizza with fresh basil"
+                "price":8.99
+            }
+        ]
+        return response(menu)
+# Dynamic menu using item model
+class ItemView(APIView):
+    def get(self, request):
+        items=Item.objects.all()
+        serializer = ItemSerializer(items,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = ItemSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+             
+
+
+
+
