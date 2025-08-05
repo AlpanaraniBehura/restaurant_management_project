962e1795631292ae245cb16d8c29857af877a6df
Alpanarani Behura <103036591+AlpanaraniBehura@users.noreply.github.com>
2025-08-05 11:33:31 +0530

    17539

--- orders/models.py
+++ orders/models.py
@@ -5,27 +5,25 @@ from products.models import Item
 # Create your models here.
 class Order(models.Model):
     STATUS_CHOICES = [
-
-        ('pending', 'pending'),
-        ('preparing', 'preparing'),
-        ('delivered', 'delivered'),
-        ('cancelled', 'cancelled'),
+         ('pending', 'pending'),
+         ('preparing', 'preparing'),
+         ('delivered', 'delivered'),
+         ('cancelled', 'cancelled'),
     ]
 
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
     total_amount = models.DecimalField(max_digits=8, decimal_places=2)
-    status = models.CharField(max_lenght=20, choices=STATUS_CHOICES, default='pending')
-    created_at = models.DecimalField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"order #{self.id} by {self.customer.username}"
+        return f"Order #{self.id} by {self.customer.username}"
 
-    class OrderItem(models.Model):
-        order = models.ForeignKey(order, on_delete=models.CASCADE, related_name='items')
-        item = models.ForeignKey(Item, on_delete=models.CASCADE)
-        quantity = models.PositiveIntegerField(default=1)
-
-       def __str__(self):
-          return f"{self.quantity} x {self.item.name} (order #{self.order.id})"
-
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+   
+    def __str__(self):
+        return f"{self.quantity} x {self.item.name} (Order #{self.order.id})"
 
