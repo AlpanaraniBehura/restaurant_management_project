a514da1564f6b5c6b0ac80d3b34760610bdd44a4
Alpanarani Behura <103036591+AlpanaraniBehura@users.noreply.github.com>
2025-08-06 16:43:47 +0530

    Auto-save home/views.py

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def homepage(request):
-    return render(request, 'home/index.html')
+    response = request.get('http://localhost:8000/api/products/menu/') # API Endpoint
+    menu_items = response.json()
+    return render(request, 'menu.html', {'menu_items': menu_items})
