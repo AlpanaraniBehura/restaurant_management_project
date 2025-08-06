67611a90cc00f03ca4934e2a8f8e5159e41bf3a0
Alpanarani Behura <103036591+AlpanaraniBehura@users.noreply.github.com>
2025-08-06 16:57:01 +0530

    Auto-save home/views.py

--- home/views.py
+++ home/views.py
@@ -3,6 +3,6 @@ import requests
 
 # Create your views here.
 def homepage(request):
-    response = request.get('http://localhost:8000/api/products/menu/') # API Endpoint
+    response = requests.get('http://localhost:8000/api/products/menu/') # API Endpoint
     menu_items = response.json()
     return render(request, 'menu.html', {'menu_items': menu_items})
