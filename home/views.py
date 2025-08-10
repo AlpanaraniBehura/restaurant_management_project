21930a76a04a5a503c53fb8d482e04db77acac38
Alpanarani Behura <103036591+AlpanaraniBehura@users.noreply.github.com>
2025-08-10 22:40:24 +0530

    27070

--- home/views.py
+++ home/views.py
@@ -3,9 +3,14 @@ import requests
 
 # Create your views here.
 def homepage(request):
+    restaurant_name = getattr(settings, "RESTAURANT_NAME", "Our Restaurant")
+try:
     response = requests.get('http://localhost:8000/api/products/menu/') # API Endpoint
+    response.raise_for_status() # Raise a error if API fails
     menu_items = response.json()
-    return render(request, 'menu.html', {'menu_items': menu_items})
+except requests.RequestException:
+    menu_items = []
+return render(request, 'menu.html', {'menu_items': menu_items})
 
 def custom_404(request, exception):
     return render(request, '404.html', status=404)
