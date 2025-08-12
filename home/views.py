2b08c3c07b9a4a253ed932eeaae374a1a856b4b5
Alpanarani Behura <103036591+AlpanaraniBehura@users.noreply.github.com>
2025-08-12 12:02:07 +0530

    27071

--- home/views.py
+++ home/views.py
@@ -1,17 +1,57 @@
-from django.shortcuts import render
-import requests
-from django.conf import settings
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{{ restaurant_name }} Menu</title>
+    <style>
+        body{
+          font-family:Arial,sans-serif;
+          margin:20px;
+          background:#fafafa;
+        }
 
-# Create your views here.
-def homepage(request):
-    restaurant_name = getattr(settings, "RESTAURANT_NAME", "Our Restaurant")
-    try:
-        response = requests.get('http://localhost:8000/api/products/menu/') # API Endpoint
-        response.raise_for_status() # Raise a error if API fails
-        menu_items = response.json()
-    except requests.RequestException:
-        menu_items = []
-        return render(request, 'menu.html', {'menu_items': menu_items, 'restaurant_name':restaurant_name})
+        nav a{
+          margin-right: 15px;
+          text-decoration: none;
+          color: #333;
+        }
+        img{
+          display:block;
+          margin:20px auto;
+          max-width:800px;
+          border-radius: 10px;
+        }
 
-def custom_404(request, exception):
-    return render(request, '404.html', status=404)
+        ul{
+          list-style:none;
+          padding:0;
+        
+        }
+
+        li{
+          margin-bottom: 15px;
+          padding: 10px;
+          background:#f4f4f4;
+          border-radius: 5px;
+        }
+    </style>
+</head>
+
+<body>
+    <nav>
+        <a href="{% url 'homepage'}">Home</a> |
+        <a href="{% url 'about' %}">About Us</a>
+    </nav>
+    <h1>{{restaurant_name}}</h1>
+    <h2>Our Menu</h2>
+    <ul>
+        {% for item in menu_items %}
+        <li>
+            <strong>{{ item.name }}</strong> - Rs.{{ item.price }}<br> {{ item.description }}
+        </li>
+        {% empty %}
+        <li>No Menu items available.</li>
+        {% endfor %}
+    </ul>
+</body>
+</html>
+          
