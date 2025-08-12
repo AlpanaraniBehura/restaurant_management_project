8ef056987676da8d38e477af766d27753e55954c
Alpanarani Behura <103036591+AlpanaraniBehura@users.noreply.github.com>
2025-08-12 14:00:57 +0530

    31095

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from home.views import homepage, about
 urlpatterns = [
     path('', homepage, name='homepage'),
     path('about/',about, name='about'),
+    path('menu-list/', views.menu-list, name='menu-list'),
     
 ]
