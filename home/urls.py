672b942b084af334298babaea8864b1876fce8eb
Alpanarani Behura <103036591+AlpanaraniBehura@users.noreply.github.com>
2025-08-18 14:07:29 +0530

    33617

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-import views
+from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('about/',views.about, name='about'),
-    path('menu-list/', views.menu-list, name='menu-list'),
+    path('menu-list/', views.menu_list, name='menu_list'),
     path('contact-us/', views.contact_us, name='contact_us'),
     path('reservations/', views.reservations, name='reservations'),
     
