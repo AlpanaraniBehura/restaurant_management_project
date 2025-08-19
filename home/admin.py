97289a5d7464d5db2326874c928b13c7e01233c0
Alpanarani Behura <103036591+AlpanaraniBehura@users.noreply.github.com>
2025-08-19 13:43:12 +0530

    34715

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Menu
+from .models import Menu, Restaurant, Customer, Feedback
 
 # Register your models here.
 admin.site.register(Menu)
+admin.site.register(Restaurant)
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'email', 'created_at')
+    search_fields = ('name', 'phone', 'email') 
+admin.site.register(Feedback)
+
