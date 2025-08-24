7a569a02a4d2d744effda6ac3a815141c729682f
Alpanarani Behura <103036591+AlpanaraniBehura@users.noreply.github.com>
2025-08-24 14:32:58 +0530

    37126

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from home.views import homepage,custom_404
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('home.urls')),
@@ -26,3 +28,5 @@ urlpatterns = [
     ]
 handler404='home.views.custom_404'
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
