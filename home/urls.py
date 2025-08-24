82950ce15a91e5ebab5c0510f28728f9517bb95b
Alpanarani Behura <103036591+AlpanaraniBehura@users.noreply.github.com>
2025-08-24 14:23:47 +0530

    37126

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('about/',views.about, name='about'),
-    path('menu-list/', views.menu_list, name='menu_list'),
+    path('menu_list/', views.menu_list, name='menu_list'),
     path('contact-us/', views.contact_us, name='contact_us'),
     path('reservations/', views.reservations, name='reservations'),
     path('feedback/', views.feedback_view, name='feedback'),
