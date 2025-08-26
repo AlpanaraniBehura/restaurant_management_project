4765e5354effbe1cd9fb3f377b5175e550cf0457
Alpanarani Behura <103036591+AlpanaraniBehura@users.noreply.github.com>
2025-08-26 14:30:12 +0530

    39439

--- home/models.py
+++ home/models.py
@@ -14,7 +14,7 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=255)
     owner_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=20)
+    phone = models.CharField(max_length=15, blank=True, null=True ) # Added field
     address = models.TextField()
     city = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
