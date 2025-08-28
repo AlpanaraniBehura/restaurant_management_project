bb4ee6d89a6fe736dbbdfe644d524c0f84e47fab
Alpanarani Behura <103036591+AlpanaraniBehura@users.noreply.github.com>
2025-08-28 10:31:43 +0530

    39435

--- forms.py
+++ forms.py
@@ -7,4 +7,14 @@ class FeedbackForm(forms.ModelForm):
         fields = ['comment']
         widgets = {
             'comment': forms.Textarea(attrs={'rows': 4, 'cols': 40, 'placeholder': 'Enter your feedback here....'})
-        }
+class ContactForm(forms.Form):
+        name = forms.CharField(max_length=100, required=True)
+        email = forms.EmailField(required=True) # Built-in email validation
+        message = forms.CharField(widget=forms.Textarea, required=True)
+
+        def clean_message(self):
+            message = self.cleaned_data.get("message", "")
+            if len(message.strip()) < 10: # ensure message is meanigful
+               raise forms.ValidationError("Message must be at least 10 characters long.")
+            return message 
+               
